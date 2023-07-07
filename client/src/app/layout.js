310a109e3eddffe41a472c0f8e6c3b9381397f53
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2023-07-07 19:48:20 +0200

    add gh link

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import Link from "next/link";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
@@ -11,6 +12,11 @@ export default function RootLayout({ children }) {
           <h1 className="p-4 text-2xl text-center font-black">EMOGUESSR</h1>
         </header>
         <main className="h-full flex flex-col">{children}</main>
+        <footer className="flex justify-center">
+          <Link className="text-lg underline" href="/https://github.com/impucky/emo-guessr/">
+            Github
+          </Link>
+        </footer>
       </body>
     </html>
   );
