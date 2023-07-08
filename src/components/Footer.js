a82e62ae30b7b84e6f6b5c077f9ffcd09380f877
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2023-07-08 16:51:20 +0200

    '

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,18 @@ export default function Footer() {
   return (
     <footer className="p-2 flex justify-center gap-8 text-sm">
       <span>
-        <i>It's not a phase, mom!</i>
+        <i>It&apos;s not a phase, mom!</i>
       </span>
-      <Link className="underline" href="https://github.com/impucky/emo-guessr/" target="_blank">
+      <Link
+        className="underline text-maroon"
+        href="https://github.com/impucky/emo-guessr/"
+        target="_blank"
+      >
         Github
       </Link>
       <div>
         Heavily inspired by{" "}
-        <Link className="underline" href="https://emovicon.app/">
+        <Link className="underline text-maroon" href="https://emovicon.app/">
           Emovicon
         </Link>
       </div>
